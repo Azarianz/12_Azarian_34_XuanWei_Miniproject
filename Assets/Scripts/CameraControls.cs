8dd8ca681bd931c7cc8c7e6bdfe0e5057ce0445f
Azarianz <62070676+Azarianz@users.noreply.github.com>
2020-08-17 17:39:52 +0800

    Added project

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    public GameObject level;
+
+    public float camrotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            level.transform.Rotate(0, -camrotationSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            level.transform.Rotate(0, camrotationSpeed * Time.deltaTime, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform camPivot;
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Camera-relative movement
+        Vector3 inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        Vector3 camF = (camPivot.forward).normalized;
+        Vector3 camR = (camPivot.right).normalized;
+        camF.y = 0;
+        camR.y = 0;
+
+        transform.position += (camF * inputMovement.z + camR * inputMovement.x) * movementSpeed * Time.deltaTime;
+
+        //Make the player always face the camera
+        transform.rotation = Quaternion.LookRotation(camF);
+
+
+    }
+}
